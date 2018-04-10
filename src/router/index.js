cf5dba77ad280e5ec035a0e4c19e8deba55481e7
lipengzhou <lpzmail@163.com>
2018-04-10 17:37:19 +0800

    添加登陆和home组件并配置路由导航

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login' // @ 是 src 路径的别名，webpack 配置的
+import Home from '@/components/home/home' // @ 是 src 路径的别名，webpack 配置的
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
+      path: '/',
+      component: Home
+    },
   ]
 })
