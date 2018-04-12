89fabeb356e5cd7756ff0d7940458148ecdc2e37
lipengzhou <lpzmail@163.com>
2018-04-12 15:50:31 +0800

    登陆成功消息提示

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -49,6 +49,10 @@ export default {
         this.$router.push({
           name: 'home'
         })
+        this.$message({
+          type: 'success',
+          message: '登陆成功!'
+        })
       }
     }
   }
