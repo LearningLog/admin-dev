6a500f2fa81a6f48961cd27f7dcbfd0c53872a2d
lipengzhou <lpzmail@163.com>
2018-04-13 16:37:28 +0800

    处理用户列表分页效果

--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -37,14 +37,27 @@
       label="电话">
     </el-table-column>
   </el-table>
+  <!--
+    你要做的就是告诉这个分页组件：
+    每页多大
+        :page-sizes="[1, 2, 3, 4]" 每页大小
+        一共有多少条记录 :total="10"
+    分页组件的页码发生改变的时候会自动调用 handleCurrentChange
+    所以我们要做的就是在页码发生改变的时候 handleCurrentChange 方法中加载对应页码的数据
+
+    当用户选择切换页码改变的时候，就会触发 handleSizeChange 方法执行
+    我们要做的就是在 handleSizeChange 方法中发请求拿到对应的页码数据
+        loadUsersByPage(1, 每页大小)
+
+    current-page 用来配置指定页码高亮
+   -->
   <el-pagination
     @size-change="handleSizeChange"
     @current-change="handleCurrentChange"
-    :current-page="4"
-    :page-sizes="[100, 200, 300, 400]"
-    :page-size="100"
+    :current-page.sync="currentPage"
+    :page-sizes="[1, 2]"
     layout="total, sizes, prev, pager, next, jumper"
-    :total="400">
+    :total="totalSize">
   </el-pagination>
 </div>
 </template>
@@ -52,31 +65,63 @@
 <script>
 export default {
   async created () {
-    // 1. 请求地址写死了，不好改
-    // 2. 每次使用 axios 都手动 import 非常麻烦
-    //    我们可以把 axios 实例注入到 Vue 实例中
-    //    然后我们就可以通过 Vue 组件实例来访问 axios 实例了
-    // 3. 除了登陆接口不需要授权用户令牌，其它都需要
-    const res = await this.$http.get('/users', {
-      params: { // 请求参数，对象会被转换为 k=v&k=v 的格式，然后拼接到请求路径 ? 后面发起请求
-        pagenum: 1,
-        pagesize: 5
-      }
-    })
-    this.tableData = res.data.data.users
+    // 第一次进来请求分页数据：第1页，每页2条
+    this.loadUsersByPage(1, 1)
   },
   data () {
     return {
       searchText: '',
-      tableData: []
+      tableData: [], // 表格列表数据
+      totalSize: 0, // 总记录数据
+      currentPage: 1, // 当前页码
+      pageSize: 1 // 当前每页大小
     }
   },
   methods: {
-    handleSizeChange (val) {
-      console.log(`每页 ${val} 条`)
+    handleSizeChange (pageSize) {
+      console.log('每页大小：', pageSize)
+
+      // 将用户选择的页码大小存储起来
+      // 用于页码改变之后，再次点击页码的时候获取最新用户选择的每页大小
+      this.pageSize = pageSize
+
+      // 页码发生改变
+      // 重新请求列表数据
+      // 用户改变每页大小之后，我们请求新的数据，以新的每页大小的第1页为准
+      this.loadUsersByPage(1, pageSize)
+
+      // 页码改变之后，不仅让数据到了第1页
+      // 同时也要让页码高亮状态也跑到第1页
+      this.currentPage = 1
     },
-    handleCurrentChange (val) {
-      console.log(`当前页: ${val}`)
+    handleCurrentChange (currentPage) {
+      console.log('页码：', currentPage)
+
+      // 将 currentPage 更新为最新点击的页码
+      // Element 插件的页码发生改变的时候，不会修改我们的数据 currentPage
+      // 我们这里让其每一次页码变化的时候，手动将 currentPage 赋值为当前最新页码
+      // this.currentPage = currentPage
+
+      // 页码改变，请求当前页码对应的数据
+      // 注意：这里我们请求的每页大小先写死
+      //       为什么呢？
+      //       我们的每页大小是会变化的
+      this.loadUsersByPage(currentPage, this.pageSize)
+    },
+
+    // 根据页码加载用户列表数据
+    async loadUsersByPage (page, pageSize = 1) {
+      const res = await this.$http.get('/users', {
+        params: { // 请求参数，对象会被转换为 k=v&k=v 的格式，然后拼接到请求路径 ? 后面发起请求
+          pagenum: page,
+          pagesize: pageSize
+        }
+      })
+      const {users, total} = res.data.data
+      this.tableData = users
+      // 请求数据成功，我们从服务器得到了总记录数据
+      // 然后我们就可以把总记录数据交给分页插件来使用
+      this.totalSize = total
     }
   }
 }
