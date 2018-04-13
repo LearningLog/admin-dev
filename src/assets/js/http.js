1d861ac6d294240531bce43baaebc6114d20b3f2
lipengzhou <lpzmail@163.com>
2018-04-13 12:02:44 +0800

    将axios扩展为Vue插件

---
+++ src/assets/js/http.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+// 建议通过定义插件的配置来扩展 Vue 本身
+// 1. 定义一个插件对象
+const httpPlugin = {}
+
+// 2. 为插件对象添加一个成员：install
+//    install 是一个函数
+//    该函数接收两个参数：Vue、options
+httpPlugin.install = function (Vue, options) {
+  // 3. 添加实例方法
+  Vue.prototype.$http = axios.create({
+    baseURL: 'http://localhost:8888/api/private/v1/'
+  })
+}
+
+// 4. 导出插件对象
+export default httpPlugin
+
+// 5. 在入门文件模块 main.js 加载使插件生效
+// Vue.use(httpPlugin)
